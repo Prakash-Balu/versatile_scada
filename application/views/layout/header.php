455352aa302d089f19de97f5787fa9c0e74ba4d0
Prakash-Balu <prakash.engg89@gmail.com>
2018-12-31 22:46:23 +0530

    logo changes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -47,7 +47,7 @@ foreach($region_list as $list)
         <span class="navbar-toggler-icon"></span>
       </button>
       <a class="navbar-brand" href="#">
-        <img class="navbar-brand-full" src="<?php echo base_url();?>assets/images/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-full" src="<?php echo base_url();?>assets/images/Ver.jpg" width="125" height="50" alt="CoreUI Logo">
         <img class="navbar-brand-minimized" src="<?php echo base_url();?>assets/images/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </a>
       <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
