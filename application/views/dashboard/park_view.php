d591392879a8a6127dfb8000141d26074ba61ae4
Prakash-Balu <prakash.engg89@gmail.com>
2019-01-02 23:24:50 +0530

    park view changes

--- application/views/dashboard/park_view.php
+++ application/views/dashboard/park_view.php
@@ -3,50 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 // echo "<pre>"; print_r($parkview); exit;
 ?>
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Park View</h3>
-              </div>
-              
-              <!--<div class="title_right">
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                  </div>
-                </div>
-              </div>-->
-            </div>
 
-            <div class="clearfix"></div>
-
+<main class="main">
+        <!-- Breadcrumb-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">Home</li>
+          <li class="breadcrumb-item">
+            <a href="#">Admin</a>
+          </li>
+          <li class="breadcrumb-item active">Park View</li>
+        </ol>
+        <div class="container-fluid">
+          <div class="animated fadeIn">
+            <?php 
+                  foreach ($parkview['regions'] as $key => $value) {
+                    foreach ($parkview['regionDeviceData'] as $key1 => $value1) {
+                      if( $value['Region'] == $key1 && array_key_exists($value['Device_Name'], $value1) ) {
+                  ?>
             <div class="row">
-              <?php foreach ($parkview['regions'] as $key => $value) {
-                foreach ($parkview['regionDeviceData'] as $key1 => $value1) {
-                  
-
-                    if( $value['Region'] == $key1 && array_key_exists($value['Device_Name'], $value1) ) {
-// echo "<pre>"; print_r($key); 
-//                     echo "<pre>"; print_r($value); 
-//                     echo "<pre>"; print_r($key1); 
-//                     echo "<pre>"; print_r($value1); exit;
-                    
-                ?>
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2><?php echo $value['Region'];?></h2>
-                     <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    
-                    <table id="table1" class="table table-striped table-bordered nowrap data_table" style="width:100%">
-                      <thead>
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header"><?php echo $value['Region'];?></div>
+                  <div class="card-body">
+                    <table class="table table-responsive-sm table-hover table-outline mb-0">
+                      <thead class="thead-light">
                         <tr>
                           <th>Power</th>
                           <th>Windspeed</th>
@@ -67,19 +47,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </tr>
                       </tbody>
                     </table>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Device info</h2>
-                      <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <table id="table2" class="table table-striped table-bordered nowrap data_table" style="width:100%">
-                      <thead>
+                    <br/>
+                    <table class="table table-responsive-sm table-hover table-outline mb-0">
+                      <thead class="thead-light">
                         <tr>
                           <th>Loc no</th>
                           <th>Status</th>
@@ -99,7 +69,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <tr>
                           <td><?php echo $value['LOC_No'];?></td>
                           <td><?php echo $value1[$value['Device_Name']][0]['Status'];?></td>
-                          <td><?php echo $value['capacity'];?></td>
+                          <td><?php echo $value['capacity'] ? $value['capacity']: 0;?></td>
                           <td><?php echo $value1[$value['Device_Name']][0]['Power'];?></td>
                           <td><?php echo $value1[$value['Device_Name']][0]['Windspeed'];?></td>
                           <td><?php echo $value1[$value['Device_Name']][0]['RRPM'];?></td>
@@ -112,48 +82,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </tr>
                       </tbody>
                     </table>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Active Alarams</h2>
-                     <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
                     
-                    <table  id="table3" class="table table-striped table-bordered nowrap data_table" style="width:100%">
-                      <thead>
-                        <tr>
-                          <th>Date</th>
-                          <th>Stop Time</th>
-                          <th>Turbine Name</th>
-                          <th>Error Description</th>
-                        </tr>
-                      </thead>
-
-
-                      <tbody>
-                        <tr>
-                          <td>6400</td>
-                          <td>100 m/s</td>
-                          <td>12</td>
-                          <td>61</td>
-                        </tr>
-                      </tbody>
-                    </table>
                   </div>
                 </div>
               </div>
+              <!-- /.col-->
+            </div>
+            <!-- /.row-->
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="card">
+                    <div class="card-header">Active Alarams</div>
+                      <div class="card-body">
+                        <table class="table table-responsive-sm table-hover table-outline mb-0">
+                          <thead class="thead-light">
+                            <tr>
+                              <th>Date</th>
+                              <th>Stop Time</th>
+                              <th>Turbine Name</th>
+                              <th>Error Description</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>6400</td>
+                              <td>100 m/s</td>
+                              <td>12</td>
+                              <td>61</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.row-->
               <?php
+                    }
                   }
                 }
-              }
-            ?>
-            </div>
-           
+              ?>
+            
           </div>
         </div>
+      </main>
         <!-- /page content -->
 <?php  $this->load->view('layout/footer'); ?>
